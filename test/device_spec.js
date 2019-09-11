7434abc1f465c118cc8b3efaf3aca9c3a0f3c8f5
naimo84 <git@neumann-benjamin.de>
2019-09-11 08:05:00 +0200

    test: fixed wrong confignode

--- test/device_spec.js
+++ test/device_spec.js
@@ -3,8 +3,6 @@ var helper = require("node-red-node-test-helper");
 
 helper.init(require.resolve('node-red'));
 
-
-
 describe('device Node', function () {
 
     beforeEach(function (done) {
@@ -20,19 +18,15 @@ describe('device Node', function () {
     it('should be loaded', function (done) {
         var flow = [
             { id: "c1", type: "fakeroku-config" },
-            { id: "n1", type: "fakeroku-device", config: "c1" }
+            { id: "n1", type: "fakeroku-device", confignode: "c1" }
         ];
         var fakerokuContainersNode = require("../dist/fakeroku-device.js");
         var fakerokuConfigNode = require("../dist/fakeroku-config.js");
 
-
-
         helper.load([fakerokuConfigNode, fakerokuContainersNode], flow, function () {
-            var n1 = helper.getNode("n1");
+            var n1 = helper.getNode("n1");           
             n1.should.have.property('type', 'fakeroku-device');
             done();
         });
-    });
-
-   
+    });   
 });
